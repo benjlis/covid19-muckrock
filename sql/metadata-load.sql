725efc5dbb5add82717f3db03aed6e20d6650129
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-08 17:37:42 -0400

    Added replace of true on org; updated comments

--- sql/metadata-load.sql
+++ sql/metadata-load.sql
@@ -5,12 +5,10 @@ select id::int, canonical_url, created_at::timestamp with time zone, language, o
        original_extension, page_count::int, slug, title, updated_at::timestamp with time zone, userinfo
 	from covid19_muckrock.metadata_stage;
 
--- extract elements out of json
+-- extract name elements out of json and overwrite field
+update metadata set organization = replace(organization, 'True', 'true');
 update metadata set organization = replace(organization, 'False', 'false');
 update metadata set organization = replace(organization, '''', '"')::jsonb ->> 'name';
 update metadata set userinfo = replace(userinfo, 'True', 'true');
 update metadata set userinfo = replace(userinfo, 'False', 'false');
 update metadata set userinfo = replace(userinfo, '''', '"')::jsonb ->> 'name';
-
--- select replace(organization, '''', '"')::jsonb ->> 'name' from metadata;
--- select replace(userinfo, '''', '"')::jsonb from metadata limit 5;

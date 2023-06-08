a6e9f1e58b21565bef1d38ed85af64333fad7d28
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-08 17:33:15 -0400

    Updated to reflect separation of ddl and load

--- sql/build.sql
+++ sql/build.sql
@@ -1,4 +1,6 @@
 create schema if not exists covid19_muckrock;
-\i metadata-stage.sql
-\i metadata.sql
+\i metadata-stage-ddl.sql
+\i metadata-stage-load.sql
+\i metadata-ddl.sql
+\i metadata-load.sql
 \i pdfs.sql

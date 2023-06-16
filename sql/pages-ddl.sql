a9271e6c9c8747b22fc289b4f5ff8630b61d3310
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-16 11:43:18 -0400

    Added downloaded column

--- sql/pages-ddl.sql
+++ sql/pages-ddl.sql
@@ -4,6 +4,7 @@ create table covid19_muckrock.pages (
     pg              integer     not null,
     word_cnt        integer     not null,
     char_cnt        integer     not null,
+    downloaded      timestamp with time zone not null default now(),
     body            text,
     primary key (dc_id, pg)
     );

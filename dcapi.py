3524b8dd1f020e509e0be16dd7e33be0e1cea18e
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-15 20:38:38 -0400

    Added download_page_text() and renew_api_token()

--- dcapi.py
+++ dcapi.py
@@ -1,15 +1,39 @@
-# import time
+import time
 import os
 import requests
+
+# automatically throttle your requests to 10 per second to avoid going over the rate limit
+DC_THROTTLE_INTERVAL = .1
 
 # get access and refresh tokens
 DC_USER = os.getenv('MR_USER')
 DC_PSWD = os.getenv('MR_PSWD')
 access_endpoint = "https://accounts.muckrock.com/api/token/"
 params = {'username': DC_USER, 'password': DC_PSWD}
-response = requests.post(access_endpoint, data = params )
+response = requests.post(access_endpoint, data=params)
 refresh_token = response.json()["refresh"]
 access_token = response.json()["access"]
+headers = {'Authorization': f'Bearer {access_token}'}
+
+def renew_api_token():
+    time.sleep(DC_THROTTLE_INTERVAL)
+    renew_endpoint = "https://accounts.muckrock.com/api/refresh/"
+    params = {'refresh': refresh_token}
+    r = requests.post(renew_endpoint, data=params)
+    global access_token, headers
+    access_token = r.json()["access"]
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+def download_page_text(page_text_endpoint):
+    time.sleep(DC_THROTTLE_INTERVAL)
+    r = requests.get(page_text_endpoint, headers=headers)
+    if (r.status_code == 403 or r.status_code == 429):
+       print(f'renewing token: {r.status_code} on {page_text_endpoint}')       
+       renew_api_token()
+       r = requests.get(page_text_endpoint, headers=headers)
+    r.raise_for_status()
+    return r.text
 
 if __name__ == '__main__':
-    print (f'{access_token=} \n{refresh_token=}')
+    # print (f'{access_token=} \n{refresh_token=}')
+    download_page_text('https://s3.documentcloud.org/documents/20488670/pages/sitka-p2.txt')

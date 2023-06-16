33039e988a888cba9f788d2dfb9af053f14b613c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-15 20:30:36 -0400

    Removed pdfs table; rename pdfpages to pages

--- sql/pages.sql
+++ sql/pages.sql
@@ -1,14 +1,6 @@
-create table covid19_muckrock.pdfs (
-    dc_id           integer     primary key
+create table covid19_muckrock.pages (
+    dc_id           integer     not null
                     references  covid19_muckrock.metadata,
-    pg_cnt          integer     not null,
-    size            integer     not null
-    );
-comment on column covid19_muckrock.pdfs.size is 'Size of PDF in bytes';
-
-create table covid19_muckrock.pdfpages (
-    dc_id          integer     not null
-                    references  covid19_muckrock.pdfs,
     pg              integer     not null,
     word_cnt        integer     not null,
     char_cnt        integer     not null,

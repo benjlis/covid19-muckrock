9d4e80e2c2dd7ba9e53bc601383361f820f6c0ae
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-16 08:12:02 -0400

    Updated call to pages-ddl

--- sql/build.sql
+++ sql/build.sql
@@ -3,7 +3,7 @@ create schema if not exists covid19_muckrock;
 \i metadata-stage-load.sql
 \i metadata-ddl.sql
 \i metadata-load.sql
-\i pdfs.sql
+\i pages-ddl.sql
 -- grants
 grant usage on schema covid19_muckrock to c19ro;
 grant select on covid19_muckrock.metadata to c19ro;

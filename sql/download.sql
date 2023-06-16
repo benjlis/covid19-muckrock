393c789d3db55eeb0cc699cfe38285ed93406341
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-16 11:46:57 -0400

    Added add-page

--- sql/download.sql
+++ sql/download.sql
@@ -1,5 +1,11 @@
 -- name: get-doc-download-list
 -- Get list of all pdfs that can be processed
 select row_number() over (order by d.doc_id), 
-       d.doc_id, d.pgtxt_prefix
+       d.doc_id, d.pg_cnt, d.pgtxt_prefix
     from covid19_muckrock.docs d;
+    -- where doc_id = 23824253;
+
+-- name: add-page!
+-- Add page of text to database
+insert into covid19_muckrock.pages(dc_id, pg, word_cnt, char_cnt, body)
+values (:id, :pg, :word_cnt, :char_cnt, :body);

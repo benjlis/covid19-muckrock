ab715ee7f10c925dfce1ce07931c0dcdc304640b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-17 19:59:30 -0400

    Exclude docs already processed from download

--- sql/download.sql
+++ sql/download.sql
@@ -2,8 +2,10 @@
 -- Get list of all pdfs that can be processed
 select row_number() over (order by d.doc_id), 
        d.doc_id, d.pg_cnt, d.pgtxt_prefix
-    from covid19_muckrock.docs d;
-    -- where doc_id = 23824253;
+    from covid19_muckrock.docs d
+    where not exists (select 1
+                                from covid19_muckrock.pages p
+                                where p.dc_id = d.dc_id);
 
 -- name: add-page!
 -- Add page of text to database

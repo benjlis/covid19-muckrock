223f6a35ee0240ea4abc881b6eeb961842673838
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-21 19:00:35 -0400

    Added add-pii

--- sql/py.sql
+++ sql/py.sql
@@ -12,9 +12,15 @@ select row_number() over (order by d.doc_id),
 select row_number() over (order by p.dc_id), 
        p.dc_id, p.pg, p.body
     from covid19_muckrock.pages p
-    order by p.dc_id, p.pg;
+    order by p.dc_id, p.pg
+    limit 1000;
 
 -- name: add-page!
 -- Add page of text to database
 insert into covid19_muckrock.pages(dc_id, pg, word_cnt, char_cnt, body)
 values (:id, :pg, :word_cnt, :char_cnt, :body);
+
+-- name: add-pii!
+-- Add pii element to database
+insert into covid19_muckrock.pii(dc_id, pg, pii_type, pii_text, start_idx, end_idx)
+values (:id, :pg, :pii_type, :pii_text, :start_idx, end_idx);

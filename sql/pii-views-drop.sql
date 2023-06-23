d87a9996a00397d0de963cb70139a8e702410a42
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-23 12:34:40 -0400

    Initial commit

---
+++ sql/pii-views-drop.sql
@@ -0,0 +1,7 @@
+drop view if exists covid19_muckrock.pii_summary;
+drop view if exists covid19_muckrock.pii_docs;
+drop view if exists covid19_muckrock.pii_docs_ext;
+drop view if exists covid19_muckrock.pii_top_emails;
+drop view if exists covid19_muckrock.pii_top_phones;
+drop view if exists covid19_muckrock.pii_top_zips;
+drop view if exists covid19_muckrock.pii_top_addresses;

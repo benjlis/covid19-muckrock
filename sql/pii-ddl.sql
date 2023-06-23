915bf1b4a090c499c3c699ba4e73f7ded69fb5e3
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-23 15:03:50 -0400

    Added pii.detected column

--- sql/pii-ddl.sql
+++ sql/pii-ddl.sql
@@ -1,9 +1,10 @@
 create table if not exists covid19_muckrock.pii 
-    (pii_id      int generated always as identity primary key,
+    (pii_id      int generated always as identity primary key,  
      dc_id       int not null,
      pg          int not null,
      pii_type    text not null references covid19_muckrock.pii_types,
      pii_text    text not null,
      start_idx   int not null,
-     end_idx     int not null, 
+     end_idx     int not null,
+     detected    timestamp with times zone defailt now(), 
      foreign key (dc_id, pg) references covid19_muckrock.pages);

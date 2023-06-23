20a007a051169e5496ff7a8e63c158dd9a3b2b88
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-23 13:07:37 -0400

    Initial commit

---
+++ sql/pii-views-copy.sql
@@ -0,0 +1,6 @@
+\copy (select * from covid19_muckrock.pii_summary) to '../tmp/pii-summmary.csv' header csv
+\copy (select * from covid19_muckrock.pii_docs_ext) to '../tmp/pii-docs-ext.csv' header csv
+\copy (select * from covid19_muckrock.pii_top_emails) to '../tmp/pii-top-emails.csv' header csv
+\copy (select * from covid19_muckrock.pii_top_phones) to '../tmp/pii-top-phones.csv' header csv
+\copy (select * from covid19_muckrock.pii_top_zips) to '../tmp/pii-top-zips.csv' header csv
+\copy (select * from covid19_muckrock.pii_top_addresses) to '../tmp/pii-addresses.csv' header csv

57442670b4461119dcab4512f0577d372a595981
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-28 13:01:53 -0400

    Added dq*duplicates views.

--- sql/dq.sql
+++ sql/dq.sql
@@ -10,3 +10,18 @@ select doc_id, title, pg_cnt, count(pg) pg_cnt_sparse,
     group by doc_id, title, pg_cnt, canonical_url, pdf_url
     order by count(pg)/pg_cnt::float desc, pg_cnt desc;
 
+
+drop view if exists covid19_muckrock.dq_body_md5_duplicates;
+create or replace view covid19_muckrock.dq_body_md5_duplicates as
+select body_md5, count(body_md5) copies
+   from covid19_muckrock.pages
+   group by body_md5
+   having count(body_md5) > 1;
+
+drop view if exists covid19_muckrock.dq_docpages_duplicates;
+create or replace view covid19_muckrock.dq_docpages_duplicates as
+select dp.*, dup.copies
+   from covid19_muckrock.docpages dp join dq_body_md5_duplicates dup
+      on (dp.body_md5 = dup.body_md5)
+   order by dp.body_md5, dp.doc_id, dp.pg;
+

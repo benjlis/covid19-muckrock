3ca659a0b4e341541d2eb88d90df0a9378e1f96d
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-28 11:11:31 -0400

    Added body_md5 column

--- sql/docpages-ddl.sql
+++ sql/docpages-ddl.sql
@@ -1,7 +1,8 @@
 -- metadata view
 drop view if exists covid19_muckrock.docpages;
 create view covid19_muckrock.docpages as
-select d.*, p.pg, p.word_cnt, p.char_cnt, p.downloaded, p.body 
+select d.*, p.pg, p.word_cnt, p.char_cnt, p.downloaded, 
+       p.body, p.body_md5 
     from covid19_muckrock.docs d join covid19_muckrock.pages p 
         on (d.dc_id = p.dc_id);
 

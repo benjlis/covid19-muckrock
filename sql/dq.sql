ea8d067d8be8691af53d0c63c8d2cbd60593ac01
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-28 13:19:32 -0400

    Added dq_sparse_docpages

--- sql/dq.sql
+++ sql/dq.sql
@@ -10,6 +10,13 @@ select doc_id, title, pg_cnt, count(pg) pg_cnt_sparse,
     group by doc_id, title, pg_cnt, canonical_url, pdf_url
     order by count(pg)/pg_cnt::float desc, pg_cnt desc;
 
+drop view if exists covid19_muckrock.dq_sparse_docpages;
+create or replace view covid19_muckrock.dq_sparse_docpages as
+select *
+    from covid19_muckrock.docpages
+    where word_cnt <= 1
+    order by doc_id, pg;
+
 
 drop view if exists covid19_muckrock.dq_body_md5_duplicates;
 create or replace view covid19_muckrock.dq_body_md5_duplicates as

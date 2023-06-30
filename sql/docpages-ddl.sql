5f897e9a8d52374de06a70e83490c1a24014709a
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-30 18:51:46 -0400

    Fixed syntax error and formatting

--- sql/docpages-ddl.sql
+++ sql/docpages-ddl.sql
@@ -1,8 +1,13 @@
 -- metadata view
 drop view if exists covid19_muckrock.docpages;
-create view covid19_muckrock.docpages as
+create or replace view covid19_muckrock.docpages as
 select d.*, p.pg, p.word_cnt, p.char_cnt, p.downloaded, 
-       p.body, p.body_md5 
-    from covid19_muckrock.docs d join covid19_muckrock.pages p 
-        on (d.dc_id = p.dc_id);
+       p.body, p.body_md5,
+       case when pe.page_id is null then 'N' else 'Y'
+       end exception 
+    from covid19_muckrock.docs d 
+            join covid19_muckrock.pages p 
+                                    on (d.dc_id = p.dc_id)
+            left join covid19_muckrock.page_exceptions pe
+                on (p.page_id = pe.page_id);
 

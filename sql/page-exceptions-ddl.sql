dd705d74ce0c63f6650d34fafcda9051f7e03cf6
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-06-30 18:35:54 -0400

    Added drop & copy commands, created column

--- sql/page-exceptions-ddl.sql
+++ sql/page-exceptions-ddl.sql
@@ -1,8 +1,10 @@
-
+drop table if exists covid19_muckrock.page_exceptions;
 create table covid19_muckrock.page_exceptions (
     page_id         integer primary key 
         references covid19_muckrock.pages,
     exception_type  text not null 
         check (exception_type in ('langdetect', 'spellcheck')),
+    created         timestamp with time zone not null default now(), 
     comments        text
     );
+\copy covid19_muckrock.page_exceptions(page_id, exception_type, comments) from 'tmp/page-exceptions-langdetect.csv' csv

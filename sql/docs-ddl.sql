6fd741a177000ed9c0e2599fbea4561dcfe25c6b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-04 18:49:19 -0400

    Added docs.api_pdf_url column

--- sql/docs-ddl.sql
+++ sql/docs-ddl.sql
@@ -1,10 +1,12 @@
 -- metadata view
 drop view if exists covid19_muckrock.docs;
-create view covid19_muckrock.docs  as
+create or replace view covid19_muckrock.docs  as
 select dc_id, dc_id doc_id, title, page_count pg_cnt, lang doc_lang, original_extension, 
     created_at, updated_at, organization, userinfo username, slug,  
     canonical_url,
 	asset_url || 'documents/' || dc_id || '/' || slug || '.pdf' pdf_url,
+    'https://api.www.documentcloud.org/files/documents/' || dc_id ||
+    '/' || slug || '.pdf'     api_pdf_url,
     'https://api.www.documentcloud.org/files/documents/' || dc_id || 
     '/pages/' || slug || '-p' pgtxt_prefix
 from covid19_muckrock.metadata;

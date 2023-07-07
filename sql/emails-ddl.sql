b1273fed6dac6b33a642f5cff970ec6d3c2a1310
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-07 09:22:39 -0400

    Added column emails.processed

--- sql/emails-ddl.sql
+++ sql/emails-ddl.sql
@@ -1,5 +1,7 @@
+drop table if exists covid19_muckrock.emails;
 create table covid19_muckrock.emails (
     email_id            integer primary key generated always as identity,
+    processed           timestamp with time zone default now(),
     dc_id               integer not null references covid19_muckrock.metadata,
     file_pg_start       integer not null,
     pg_cnt              integer not null,

8e69236916ae65887b7474580c20e87a21270dcb
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-07 09:19:13 -0400

    Initial commit

---
+++ sql/emails-ddl.sql
@@ -0,0 +1,20 @@
+create table covid19_muckrock.emails (
+    email_id            integer primary key generated always as identity,
+    dc_id               integer not null references covid19_muckrock.metadata,
+    file_pg_start       integer not null,
+    pg_cnt              integer not null,
+    header_begin_ln     integer not null,
+    header_end_ln       integer not null,
+    subject             text,
+    sent                text,  -- timestamp with time zone,
+    from_email          text,
+    to_emails           text,
+    cc_emails           text,
+    bcc_emails          text,
+    attachments         text,
+    importance          text,
+    body                text,
+    header_unprocessed  text,
+    header_validated    boolean,
+    sent_str            text
+    );

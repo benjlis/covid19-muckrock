b7e2609187dcb985cddff15ed50a45a33dcd4ff5
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-07 12:14:31 -0400

    - dc_id, file_pg_start, pg_cnt, body, + page_id

--- sql/emails-ddl.sql
+++ sql/emails-ddl.sql
@@ -2,9 +2,7 @@ drop table if exists covid19_muckrock.emails;
 create table covid19_muckrock.emails (
     email_id            integer primary key generated always as identity,
     processed           timestamp with time zone default now(),
-    dc_id               integer not null references covid19_muckrock.metadata,
-    file_pg_start       integer not null,
-    pg_cnt              integer not null,
+    page_id             integer not null references covid19_muckrock.pages,
     header_begin_ln     integer not null,
     header_end_ln       integer not null,
     subject             text,
@@ -15,7 +13,6 @@ create table covid19_muckrock.emails (
     bcc_emails          text,
     attachments         text,
     importance          text,
-    body                text,
     header_unprocessed  text,
     header_validated    boolean,
     sent_str            text

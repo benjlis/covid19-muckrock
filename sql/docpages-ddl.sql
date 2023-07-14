b657075c672ee4c8d48eee1817d77a92d4e11fef
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-13 21:02:54 -0400

    Added line_cnt and max_line_length columns

--- sql/docpages-ddl.sql
+++ sql/docpages-ddl.sql
@@ -1,7 +1,8 @@
 -- metadata view
 drop view if exists covid19_muckrock.docpages cascade;
 create or replace view covid19_muckrock.docpages as
-select d.*, p.pg, p.page_id, p.word_cnt, p.char_cnt, p.downloaded, 
+select d.*, p.pg, p.page_id, p.downloaded,  
+       p.line_cnt, p.max_line_length, p.word_cnt, p.char_cnt, 
        p.body, p.body_md5,
        case when pe.page_id is null then 'N' else 'Y'
        end exception, pe.exception_type, pe.comments exception_comments 

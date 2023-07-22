b5e0037071cf36aba006e9cd226a4b660151355d
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-22 14:23:59 -0400

    Added full_text column; reordered columns

--- sql/docpages-ddl.sql
+++ sql/docpages-ddl.sql
@@ -1,9 +1,9 @@
 -- metadata view
 drop view if exists covid19_muckrock.docpages cascade;
 create or replace view covid19_muckrock.docpages as
-select d.*, p.pg, p.page_id, p.downloaded,  
+select d.*, p.pg, p.page_id, p.body, pt.full_text, p.downloaded,  
        p.line_cnt, p.max_line_length, p.word_cnt, p.char_cnt, 
-       p.body, p.body_md5,
+       p.body_md5,
        case when p.word_cnt <= 4        then 'Y'
             when max_line_length <= 8   then 'Y'
             when pe.page_id is not null then 'Y'
@@ -19,5 +19,7 @@ select d.*, p.pg, p.page_id, p.downloaded,
             join covid19_muckrock.pages p 
                                     on (d.dc_id = p.dc_id)
             left join covid19_muckrock.page_exceptions pe
-                on (p.page_id = pe.page_id);
+                on (p.page_id = pe.page_id)
+            left join covid19_muckrock.page_tsvectors pt
+                on (p.page_id = pt.page_id);
 grant select on covid19_muckrock.docpages to c19ro;

4d12fd2ec4ab7aff7bab898eda82f53ee4092426
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-07-25 17:24:44 -0400

    Added column s3_pdf_url

--- sql/docs-ddl.sql
+++ sql/docs-ddl.sql
@@ -12,6 +12,8 @@ select m.dc_id, m.dc_id doc_id, m.title, m.page_count pg_cnt,
         '/' || m.slug || '.pdf'     api_pdf_url,
     'https://api.www.documentcloud.org/files/documents/' || m.dc_id || 
         '/pages/' || m.slug || '-p' pgtxt_prefix,
+    'https://foiarchive-covid-19.s3.amazonaws.com/muckrock/' ||
+        p.filename s3_pdf_url,
     p.version pdf_version, p.filename pdf_filename, p.title pdf_title,
     p.subject pdf_subject, p.keywords pdf_keywords,
     p.author pdf_author, p.creator pdf_creator, p.producer pdf_producer,

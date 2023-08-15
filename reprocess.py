08d00338a11d59a05b753ee0059dbf58bf16a0be
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-08-15 19:32:40 -0400

    Initial release version

--- reprocess.py
+++ reprocess.py
@@ -6,6 +6,7 @@ from   pdf_s3download import get_pdf
 from   ocrmypdf import ocr
 from   page_ner import page_ner
 from   pii_detect import detect_store_pii
+from   lang_detect import lang_eval
 import pdftotext
 
 
@@ -33,7 +34,13 @@ def reprocess_pii_detect(conn, stmts, doc_id, pg, text):
 
 
 def reprocess_page_exception(conn, stmts, page_id, text):
-    pass
+    stmts.delete_page_exception(conn, stmts, page_id=page_id)
+    problem = lang_eval(text)
+    if problem:
+        stmts.add_page_exception(conn, stmts, page_id=page_id, 
+                                 comments=str(problem))
+    else:
+        stmts.add_page_tsvector(conn, stmts, page_id=page_id)
 
 
 def main():
@@ -48,15 +55,16 @@ def main():
         pdf_filename = get_pdf(doc_id)
         for cnt, pg, page_id, exception, body in \
             stmts.get_docpage_exception_list(conn, doc_id=doc_id):
-            print(f'**** PAGE: {cnt}. pg:{pg}  exception_type: {exception}')
-            print(f'Text Before Reprocessing: \n{body}\n')
+            print(f'**** {title} ({doc_id}) PAGE: {cnt}. pg:{pg}, \
+exception: {exception}')
+            # print(f'Text Before Reprocessing: \n{body}\n')
             print(f'OCRing Page:')
             # ocrmypdf --force-ocr --pages 261 --clean  --deskew --output-type pdf  sitka.pdf ocr.pdf
             ocr(input_file=pdf_filename, output_file=pdf_filename,
                 pages=str(pg), force_ocr=True, clean=True, output_type='pdf')
             with open(pdf_filename, "rb") as f:
                 pdf = pdftotext.PDF(f, physical=True)
-            print(f'Text After Reprocessing: \n{pdf[pg-1]}\n')
+            # print(f'Text After Reprocessing: \n{pdf[pg-1]}\n')
             store_reprocessed_page(conn, stmts, page_id, pdf[pg-1])
             reprocess_ner(conn, stmts, page_id)
             reprocess_pii_detect(conn, stmts, doc_id, pg, pdf[pg-1])

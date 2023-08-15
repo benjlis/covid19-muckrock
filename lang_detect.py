3f847f4502d1b594dc9071efda45f14c93e5488c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-08-15 19:29:45 -0400

    Refactor code into lang_eval and full_eval

--- lang_detect.py
+++ lang_detect.py
@@ -6,16 +6,29 @@ import csv
 
 # langdetect configs
 langdetect.DetectorFactory.seed = 0    # ensure consistent results
-# db-related configuration
-conn = psycopg2.connect("")
-stmts = aiosql.from_path("sql/py.sql", "psycopg2")
- 
 
-pgs = stmts.get_page_list(conn)
-with open('tmp/page-exceptions-langdetect.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    for p in pgs:
-        cnt, doc_id, pg, body, page_id =  p
+
+def lang_eval(body):
+    try:
+        lang = langdetect.detect_langs(body)
+        print(f'in lang_eval: {lang}')
+        if lang[0].__dict__['lang'] != 'en' or \
+           lang[0].__dict__['prob'] <= .98: 
+                return lang
+    except Exception as e:
+            return e
+
+
+def full_eval():
+    # db-related configuration
+    conn = psycopg2.connect("")
+    stmts = aiosql.from_path("sql/py.sql", "psycopg2")
+
+    pgs = stmts.get_page_list(conn)
+    with open('tmp/page-exceptions-langdetect.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for p in pgs:
+            cnt, doc_id, pg, body, page_id =  p
         try:
             lang = langdetect.detect_langs(body)
             if lang[0].__dict__['lang'] != 'en' or \
@@ -25,3 +38,6 @@ with open('tmp/page-exceptions-langdetect.csv', 'w') as csvfile:
         except Exception as e:
             print(f'{cnt}, {doc_id}, {pg}, {page_id}: {e}')
             writer.writerow([page_id, 'langdetect', e])
+
+if __name__ == "__main__":
+     full_eval()

413752f8b35c8985ddb6b4dd3007b118ddcb7b45
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-08-18 20:05:43 -0400

    Refactored to make OCR processing much faster

--- reprocess.py
+++ reprocess.py
@@ -1,7 +1,5 @@
 import aiosql
 import psycopg2
-import boto3
-import argparse
 from   pdf_s3download import get_pdf
 from   ocrmypdf import ocr
 from   page_ner import page_ner
@@ -54,18 +52,23 @@ def main():
 
     for cnt, doc_id, title, url in stmts.get_doc_exception_list(conn):
         pdf_filename = get_pdf(doc_id)
+        # pdf_filename = '' # if failure in flight
         print(f'** DOCUMENT: {cnt}. {title} ({doc_id}): {url}')
-        for cnt, pg, page_id, exception, body in \
-            stmts.get_docpage_exception_list(conn, doc_id=doc_id):
+        reprocess =  stmts.get_docpage_exception_list(conn, doc_id=doc_id)
+        reprocess_list = list(reprocess)
+        pages = [r[1] for r in reprocess_list]
+        pages_str = ', '.join(str(p) for p in pages)
+        print(f'reprocessing {len(pages)} pages')
+        print(f'pages: {pages_str}')
+        print(f'OCRing pages:')
+        ocr(input_file=pdf_filename, output_file=pdf_filename,
+            pages=pages_str, force_ocr=True, clean=True, output_type='pdf')
+        with open(pdf_filename, "rb") as f:
+            pdf = pdftotext.PDF(f, physical=True)  
+        for cnt, pg, page_id, exception, body in reprocess_list:
             print(f'**** {title} ({doc_id}) PAGE: {cnt}. pg:{pg}, \
 exception: {exception}')
             # print(f'Text Before Reprocessing: \n{body}\n')
-            print(f'OCRing Page:')
-            # ocrmypdf --force-ocr --pages 261 --clean  --deskew --output-type pdf  sitka.pdf ocr.pdf
-            ocr(input_file=pdf_filename, output_file=pdf_filename,
-                pages=str(pg), force_ocr=True, clean=True, output_type='pdf')
-            with open(pdf_filename, "rb") as f:
-                pdf = pdftotext.PDF(f, physical=True)
             # print(f'Text After Reprocessing: \n{pdf[pg-1]}\n')
             store_reprocessed_page(conn, stmts, page_id, pdf[pg-1])
             reprocess_ner(conn, stmts, page_id)

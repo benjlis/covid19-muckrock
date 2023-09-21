9ce0070de51f40ced01d5c00751d87f019b060fd
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-09-21 08:19:07 -0400

    Added RI for wiki id, not null enttype.

--- sql/entities-ddl.sql
+++ sql/entities-ddl.sql
@@ -1,12 +1,12 @@
 create table covid19_muckrock.entities (
     entity_id   integer primary key generated always as identity,
-    wikidata_id text    not null unique,
-    entity      text,
-    enttype     text,
+    wikidata_id text    not null unique
+        references covid19_muckrock.wikidata_items,
+    entity      text    not null,
+    enttype     text    not null,
     created     timestamp with time zone not null default now(),
     updated     timestamp with time zone not null default now()
     );
--- Make entity, enttype not null after entity population
 
 create table covid19_muckrock.entity_pages (
     ei_id      integer primary key generated always as identity,
@@ -19,6 +19,6 @@ create table covid19_muckrock.entity_pages (
     created    timestamp with time zone not null default now(),
     updated    timestamp with time zone not null default now()
     );
--- add indices on entity_id, page_id upon completing load
+-- add indices on upon completing load:
 -- create index on entity_pages(entity_id);
 -- create index on entity_pages(page_id);
